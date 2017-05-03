c17493cb1d0755f5fd7dbe566e62f67d618867e5
Nikolay Kostin <nikolaymetos@gmail.com>
2017-05-03 20:41:54 +0300

    created model Jobs

--- db/migrate/20170503171248_create_jobs.rb
+++ db/migrate/20170503171248_create_jobs.rb
@@ -1,9 +1,9 @@
 class CreateJobs < ActiveRecord::Migration[5.0]
   def change
     create_table :jobs do |t|
-      t.string :name, :null => false
-      t.string :place, :null => false
-      t.integer :company_id, :null => false
+      t.string :name
+      t.string :place
+      t.integer :company_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Company delete_all
+Company reset_pk_sequence
+Company create([
+    {name:'MoGo', location: 'New York'},
+    {name:'Wirkkle', location: 'London'},
+    {name:'Artesis', location: 'Saint-Petersburg'},
+    {name:'BuildEmpire', location: 'London'},
+               ])
+
+Job delete_all
+Job reset_pk_sequence
+Job create ([
+    {name:'Sinatra React', place:'Remote', company_id:1},
+    {name:'Ruby React', place:'Remote', company_id:2},
+    {name:'React', place:'Remote', company_id:3},
+])
